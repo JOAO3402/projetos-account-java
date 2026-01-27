8a2bc34de1705d061ae2e40702ea67df9a313930
Joao Alexandre <jo.alexandre.ber@gmail.com>
2026-01-27 14:50:59 -0300

    Classes account e businessAccount criadas com herança

---
+++ src/entities/Account.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    private Double balance = 0.0;
+
+    public Account(){
+    }
+
+    public Account(Integer number, String holder, Double firstDeposit){
+        this.number = number;
+        this.holder = holder;
+        this.balance += firstDeposit;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public void withdraw(Double amount){
+        balance -= amount;
+    }
+
+    public void deposit(Double amount){
+        balance += amount;
+    }
+}
---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+    private Double loanLimit;
+
+    public BusinessAccount(){
+        super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double firstDeposit, Double loanLimit){
+        super(number, holder, firstDeposit);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(Double amount){
+        while(amount != 0){
+            if(amount <= loanLimit){
+                deposit(amount);
+            }
+        }
+    }
+}
