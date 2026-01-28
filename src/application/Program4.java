b84c0067b564e61a9f07b70b670bd301c567d0d2
Joao Alexandre <jo.alexandre.ber@gmail.com>
2026-01-28 17:54:51 -0300

    Criação do Program4 com testes de Polimorfismo

---
+++ src/application/Program4.java
@@ -0,0 +1,18 @@
+package application;
+
+import entities.Account;
+import entities.SavingsAccount;
+
+public class Program4 {
+    public static void main(String[] args) {
+
+        Account x = new Account("001", "Alex", 1000.0);
+        Account y = new SavingsAccount("002", "Maria", 1000.0, 1.0);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());--
+    }
+}
